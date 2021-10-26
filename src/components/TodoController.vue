804692b1dd28ad1370630af9dd7dc0e635f4a7cd
masahiro96848 <feetwcpic9436@gmail.com>
2021-10-26 23:10:42 +0900

    アイテム数が1のときitemを単数形にする

--- src/components/TodoController.vue
+++ src/components/TodoController.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="footer" v-show="todos.length" v-cloak>
 		<span class="todo-count">
-			<strong>{{remaining}}</strong> items left
+			<strong>{{ remaining }}</strong> {{ remaining | pluralize}} left
 		</span>
 	</footer>
 </template>
@@ -9,13 +9,18 @@
 <script>
 export default {
     name: 'TodoController',
+    filters: {
+        pluralize(n) {
+            return n === 1 ? 'item' : 'items';
+        }
+    },
     computed: {
         todos() {
             return this.$store.state.todos
         },
         remaining() {
             return this.$store.getters.remaining
-        }
+        },
     }
 }
 </script>
